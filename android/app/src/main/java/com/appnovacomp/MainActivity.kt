9f52f69ca8870b1b346abf4938771d89dc42d222
gcorredorlab <gcorredorlab@gmail.com>
2024-06-13 12:24:39 -0500

    🚧 add Bundle

--- android/app/src/main/java/com/appnovacomp/MainActivity.kt
+++ android/app/src/main/java/com/appnovacomp/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.appnovacomp
 
+import android.os.Bundle;
+
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
